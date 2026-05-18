9ddaad90836f1975492b50b1b3997eaaa876747c
ocasioluis907-boop <ocasioluis907@gmail.com>
2026-05-18 21:09:12 +0530

    video

--- src/routes/endpoint.ts
+++ src/routes/endpoint.ts
@@ -1040,6 +1040,36 @@ function scrapeRoutes() {
   router.get("/javgiga/search", cors(), slow, limiter, searchJavGiga);
   router.get("/missav/get", cors(), slow, limiter, getMissav);
   router.get("/missav/search", cors(), slow, limiter, searchMissav);
+  router.get("/video", async (req, res) => {
+    const url = req.query.url as string;
+
+    if (!url) {
+        return res.status(400).send("No URL");
+    }
+
+    try {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+
+        let contentType = "video/mp4";
+
+        if (url.includes(".m3u8")) {
+            contentType = "application/vnd.apple.mpegurl";
+        }
+
+        res.setHeader("Content-Type", contentType);
+
+        const response = await axios.get(url, {
+            responseType: "stream",
+            maxRedirects: 5,
+        });
+
+        response.data.pipe(res);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error fetching video");
+    }
+});
 
   return router;
 }
