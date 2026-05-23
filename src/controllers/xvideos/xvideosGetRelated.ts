68c3cd7fe11af9140f9cd2cdd53bcc28296e4943
ocasioluis907-boop <ocasioluis907@gmail.com>
2026-05-23 17:01:32 +0530

    Change return to send JSON response for scraped data
    Return scraped data as JSON response instead of raw data.

--- src/controllers/xvideos/xvideosGetRelated.ts
+++ src/controllers/xvideos/xvideosGetRelated.ts
@@ -6,7 +6,7 @@ export async function relatedXvideos({ query }: { query: { id: string } }) {
     const { id } = query;
     const url = `${c.XVIDEOS}/${id}`;
     const data = await scrapeContent(url);
-    return data;
+    res.json(data);
   } catch (err) {
     const e = err as Error;
     throw new Error(e.message);
