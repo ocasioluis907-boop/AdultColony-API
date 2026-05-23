4368a61420f426c86bceedafb420c3024f48e103
ocasioluis907-boop <ocasioluis907@gmail.com>
2026-05-23 17:14:06 +0530

    Refactor relatedXnxx to use Express types and logging
    Refactor relatedXnxx function to use Request and Response types, add logging, and handle errors with a JSON response.

--- src/controllers/xnxx/xnxxGetRelated.ts
+++ src/controllers/xnxx/xnxxGetRelated.ts
@@ -1,15 +1,26 @@
 import { scrapeContent } from "../../services/scrapers/xnxx/xnxxGetRelatedController";
 import c from "../../utils/options";
+import { logger } from "../../utils/logger";
+import { maybeError } from "../../utils/modifier";
+import { Request, Response } from "express";
 
-export async function relatedXnxx({ query }: { query: { id: string } }) {
+export async function relatedXnxx(req: Request, res: Response) {
   try {
-    const { id } = query;
+    const id = req.query.id as string;
+    if (!id) throw Error("Parameter id is required");
+
     const url = `${c.XNXX}/${id}`;
     const data = await scrapeContent(url);
+    logger.info({
+      path: req.path,
+      query: req.query,
+      method: req.method,
+      ip: req.ip,
+      useragent: req.get("User-Agent"),
+    });
     res.json(data);
   } catch (err) {
     const e = err as Error;
-    throw new Error(e.message);
+    res.status(400).json(maybeError(false, e.message));
   }
 }
-
