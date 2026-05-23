f2dd2ca4a2c6f0b02e0081b6862acab2acb3d546
ocasioluis907-boop <ocasioluis907@gmail.com>
2026-05-23 15:17:02 +0530

    Update xnxxGetRelatedController.ts

--- src/services/scrapers/xnxx/xnxxGetRelatedController.ts
+++ src/services/scrapers/xnxx/xnxxGetRelatedController.ts
@@ -1,11 +1,15 @@
 import { load } from "cheerio";
-import { lust } from "../../../LustPress";
+// FIX: Swapped out LustPress for your verified AdultColony engine module
+import AdultColony from "../../../AdultColony";
 import c from "../../../utils/options";
 import { ISearchVideoData, XnxxRelatedRaw } from "../../../interfaces";
 
+const adultcolony = new AdultColony();
+
 export async function scrapeContent(url: string) {
   try {
-    const res = await lust.fetchBody(url);
+    // FIX: Using the verified fetchBody engine call
+    const res = await adultcolony.fetchBody(url);
     const $ = load(res);
 
     class XnxxSearch {
