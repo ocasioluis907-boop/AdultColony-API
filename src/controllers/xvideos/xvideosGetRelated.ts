8b2d30af2414841d299c8d6836d5f752eb326310
ocasioluis907-boop <ocasioluis907@gmail.com>
2026-05-23 17:11:15 +0530

    Refactor relatedXvideos to use Express types
    Refactor relatedXvideos function to use Request and Response types. Add logging for request details and improve error handling.

--- src/controllers/xvideos/xvideosGetRelated.ts
+++ src/controllers/xvideos/xvideosGetRelated.ts
@@ -1,15 +1,27 @@
 import { scrapeContent } from "../../services/scrapers/xvideos/xvideosGetRelatedController";
 import c from "../../utils/options";
+import { logger } from "../../utils/logger";
+import { maybeError } from "../../utils/modifier";
+import { Request, Response } from "express";
 
-export async function relatedXvideos({ query }: { query: { id: string } }) {
+export async function relatedXvideos(req: Request, res: Response) {
   try {
-    const { id } = query;
+    const id = req.query.id as string;
+    if (!id) throw Error("Parameter id is required");
+
     const url = `${c.XVIDEOS}/${id}`;
     const data = await scrapeContent(url);
+    logger.info({
+      path: req.path,
+      query: req.query,
+      method: req.method,
+      ip: req.ip,
+      useragent: req.get("User-Agent"),
+    });
     res.json(data);
   } catch (err) {
     const e = err as Error;
-    throw new Error(e.message);
+    res.status(400).json(maybeError(false, e.message));
   }
 }
 
