7834de1230cfd5c1dd33cbf47e90f015734ff856
ocasioluis907-boop <ocasioluis907@gmail.com>
2026-05-24 11:41:57 +0530

    add api keep-allive

--- src/routes/endpoint.ts
+++ src/routes/endpoint.ts
@@ -1050,35 +1050,38 @@ function scrapeRoutes() {
   router.get("/missav/get", cors(), slow, limiter, getMissav);
   router.get("/missav/search", cors(), slow, limiter, searchMissav);
   router.get("/video", async (req, res) => {
-    const url = req.query.url as string;
-
-    if (!url) {
-        return res.status(400).send("No URL");
-    }
-
-    try {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-
-        let contentType = "video/mp4";
-
-        if (url.includes(".m3u8")) {
-            contentType = "application/vnd.apple.mpegurl";
-        }
-
-        res.setHeader("Content-Type", contentType);
-
-        const response = await axios.get(url, {
-            responseType: "stream",
-            maxRedirects: 5,
-        });
-
-        response.data.pipe(res);
-
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Error fetching video");
-    }
-});
+      const url = req.query.url as string;
+  
+      if (!url) {
+          return res.status(400).send("No URL");
+      }
+  
+      try {
+          res.setHeader("Access-Control-Allow-Origin", "*");
+  
+          let contentType = "video/mp4";
+  
+          if (url.includes(".m3u8")) {
+              contentType = "application/vnd.apple.mpegurl";
+          }
+  
+          res.setHeader("Content-Type", contentType);
+  
+          const response = await axios.get(url, {
+              responseType: "stream",
+              maxRedirects: 5,
+          });
+  
+          response.data.pipe(res);
+  
+      } catch (err) {
+          console.error(err);
+          res.status(500).send("Error fetching video");
+      }
+  });
+  router.get("/keep-alive", (req, res) => {
+    return res.status(200).json({ status: "ok" });
+  });
 
   return router;
 }
